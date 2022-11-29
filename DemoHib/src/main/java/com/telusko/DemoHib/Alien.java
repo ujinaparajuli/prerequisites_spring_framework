3dc25dac49880e44117e51298303a4c7ce881dc6
Ashish Poudel <ashishpoudel523@gmail.com>
2022-11-29 22:58:53 +0545

    Maven Project for HB with dependencies

---
+++ DemoHib/src/main/java/com/telusko/DemoHib/Alien.java
@@ -0,0 +1,27 @@
+package com.telusko.DemoHib;
+
+public class Alien {   //POJO
+	private int aid;
+	private String aname;
+	private String acolor;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getAcolor() {
+		return acolor;
+	}
+	public void setAcolor(String acolor) {
+		this.acolor = acolor;
+	}
+	
+	
+}
